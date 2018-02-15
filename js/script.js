8553ba635f27a7a121757200705595692adceb13
Yuriy Shandor <yuriyshandor@gmail.com>
2018-02-15 13:44:10 +0200

    Add Section HowIt. Mod_02

--- js/script.js
+++ js/script.js
@@ -12,4 +12,15 @@ $(document).ready(function() {
    autoplay: true,
    autoplaySpeed: 5000,
   });
+
+  $(".howIt__btn:nth-child(1)").addClass("howIt__btn_active");
+  $(".howIt-block__content:nth-child(1)").addClass("howIt-block__content_active");
+
+  $(".howIt__btn").click(function(){
+    $(".howIt__btn").removeClass("howIt__btn_active");
+    $(this).addClass("howIt__btn_active");
+    var howItBtnIndex = $(this).index()+1;
+    $(".howIt-block__content").removeClass("howIt-block__content_active");
+    $(".howIt-block__content:nth-child("+ howItBtnIndex +")").addClass("howIt-block__content_active");
+  });
 });
