ae2d8272d2f3eff5e9a314e3534fb59d0efede9b
Yuriy Shandor <yuriyshandor@gmail.com>
2018-02-15 10:35:59 +0200

    Add Section Baner

--- js/script.js
+++ js/script.js
@@ -3,4 +3,13 @@ $(document).ready(function() {
     $(this).toggleClass('toggler-icon_close');
     $('.nav-bar').slideToggle('0.5s');
   });
+
+  $('.baner-slider').slick({
+   slidesToShow: 1,
+   slidesToScroll: 1,
+   nextArrow: $(".baner-slider__arrow_next"),
+   prevArrow: $(".baner-slider__arrow_prev"),
+   autoplay: true,
+   autoplaySpeed: 5000,
+  });
 });
